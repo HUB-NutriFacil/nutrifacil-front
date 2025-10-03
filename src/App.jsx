171c9d01f946a5f5ad8feb38fc45def1943442c3
gabbu <gabrielle.sote@gmail.com>
2025-10-03 15:36:41 -0300

    chore: adicionando react dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import QuizPage from './features/pages/QuizPage.jsx'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    
+    
     </>
   )
 }
---
+++ src/features/pages/QuizPage.jsx
@@ -0,0 +1,9 @@
+import styles from './QuizPage.module.css'
+
+
+function QuizPage(){
+    return <h1>Quiz Page</h1>
+}
+
+
+export default QuizPage
